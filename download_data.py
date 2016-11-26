647dde0b0bff1bfdfd4603f22dd6f97cb32092d7
Keenan Burnett <keenburn2004@gmail.com>
2016-11-26 17:08:21 -0500

    Made some more changes to download_data, were going to try to run this code on sam laptop

--- download_data.py
+++ download_data.py
@@ -1,4 +1,5 @@
-from scipy.misc import imread
+import PIL
+from scipy.ndimage import imread
 import numpy as np
 import os
 
@@ -10,15 +11,15 @@ if(len(files) == 0):
 	raise ValueError('There were no pictures to be read')
 
 file = files[0]
-x = imread(file, flatten=False, mode='RGB')
+x = imread(myPath + file, flatten=False, mode='RGB')
 imageL = x.shape[0]
 imageW = x.shape[1]
 numImages = len(files)
 data = np.zeros((numImages, imageL, imageW, 3))		# Channels = 3
 
 for i in range(0, numImages):
-	x = imread(fileName, flatten=False, mode='RGB')	# returns ndarray (L x W x 3)
+	x = imread(myPath + fileName, flatten=False, mode='RGB')	# returns ndarray (L x W x 3)
 	data[i,:,:,:] = x
 
-
+np.save('array_data.txt', data)
 	
