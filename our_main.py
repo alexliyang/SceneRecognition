547eabee92328638faa858662d8cd2340ef0fc9e
Keenan Burnett <keenburn2004@gmail.com>
2016-11-27 19:16:58 -0500

    Changed k to [1,3,3,1] and step to [1,3,3,1] as well as number of filters in the first convolutional layer to 16. Test accuracy is now 0.278

--- our_main.py
+++ our_main.py
@@ -33,8 +33,8 @@ def bias_variable(shape):
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+  return tf.nn.max_pool(x, ksize=[1, 3, 3, 1],
+                        strides=[1, 3, 3, 1], padding='SAME')
 def main(_):
   mnist = read_data_sets(FLAGS.data_dir, one_hot=True, dtype=dtypes.float32)
   # Create the model
@@ -43,21 +43,21 @@ def main(_):
   b = tf.Variable(tf.zeros([8]))
   y = tf.matmul(x, W) + b
   # Define the first convolutional layer
-  W_conv1 = weight_variable([5, 5, 3, 32])
-  b_conv1 = bias_variable([32])
+  W_conv1 = weight_variable([5, 5, 3, 16])
+  b_conv1 = bias_variable([16])
   x_image = tf.reshape(x, [-1,128,128,3])
   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
   h_pool1 = max_pool_2x2(h_conv1)
   
   # Define the second convolutional layer
-  W_conv2 = weight_variable([5, 5, 32, 64])
+  W_conv2 = weight_variable([5, 5, 16, 64])
   b_conv2 = bias_variable([64])
   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
   h_pool2 = max_pool_2x2(h_conv2)
   # Define a fully connected layer
-  W_fc1 = weight_variable([32 * 32 * 64, 1024])
+  W_fc1 = weight_variable([15 * 15 * 64, 1024])
   b_fc1 = bias_variable([1024])
-  h_pool2_flat = tf.reshape(h_pool2, [-1, 32*32*64])
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 15*15*64])
   h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
   # Dropout 
   keep_prob = tf.placeholder(tf.float32)
