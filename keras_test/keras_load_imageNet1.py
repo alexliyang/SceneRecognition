90482cacd08c6ea560a349f3e0cbd9f062132fb1
Keenan Burnett <keenburn2004@gmail.com>
2016-12-01 20:25:15 -0500

    Made modifications to my code so that it will properly print our predictios to a CSV file, we also now grab all the text examples from the two different folders.

--- keras_test/keras_load_imageNet1.py
+++ keras_test/keras_load_imageNet1.py
@@ -57,17 +57,22 @@ def imageNet_model(vgg_weights_path=None, fc_weights_path=None):
     model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    vgg_layers = model.layers
+    vgg_layers = len(model.layers)
 
-    model.add(Flatten(input_shape=(512, 4, 4)))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(8, activation='softmax')) # Was sigmoid
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu', name='fc_1'))
+    model.add(Dropout(0.5))
+    model.add(Dense(8, activation='softmax', name='fc_2')) # Was sigmoid
+
+    total_layers = len(model.layers)
+    fc_layers = total_layers - vgg_layers
+    print(vgg_layers)
+    print(fc_layers)
 
     # Load the VGG weights into the model.
     f = h5py.File(vgg_weights_path)
     for k in range(f.attrs['nb_layers']):
-        if k >= len(vgg_layers):
+        if k >= vgg_layers:
             # we don't look at the last (fully-connected) layers in the savefile
             break
         g = f['layer_{}'.format(k)]
@@ -75,13 +80,15 @@ def imageNet_model(vgg_weights_path=None, fc_weights_path=None):
         model.layers[k].set_weights(weights)
     f.close()
 
-    # Load the FC weights into the model.
-    f = h5py.File(fc_weights_path)
-     for k in range(f.attrs['nb_layers']):
-        g = f['layer_{}'.format(k)]
-        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
-        model.layers[k + vgg_layers].set_weights(weights)
-    f.close()
+    # # Load the FC weights into the model.
+    # f = h5py.File(fc_weights_path)
+    # for k in range(f.attrs['nb_layers']):
+    #     g = f['layer_{}'.format(k)]
+    #     weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+    #     model.layers[k + vgg_layers].set_weights(weights)
+    # f.close()
+    model.load_weights(fc_weights_path, by_name=True)
+
 
     return model
 
@@ -90,24 +97,35 @@ if __name__ == "__main__":
     fc_path = 'bottleneck_fc_model.h5'
     cwd = os.getcwd()
     #myPath = '/Users/sam/Google Drive/School/2016-17/CSC411/a3/keras_test/data/test/'
-    my_path = cwd + '/data/test/'
+    small_path = cwd + '/data/test/'
+    big_path = cwd + '/data/test_128/'
     
     # Load the test image data into a numpy array: data[m,x,y,c]
-    files = os.listdir(myPath)
+    files = os.listdir(big_path)
+    num_big = len(files)
+    files = os.listdir(small_path)
     if(len(files) == 0):
         raise ValueError('There were no pictures to be read')
     file = files[0]
-    x = imread(myPath + file, flatten=False, mode='RGB')
+    x = imread(small_path + file, flatten=False, mode='RGB')
     x = imresize(x, (224,224))
     imageL = x.shape[0]
     imageW = x.shape[1]
-    numImages = len(files)
-    data = np.zeros((numImages, imageL, imageW, 3))     # Channels = 3
-    for i in range(0, numImages): 
+    num_small = len(files)
+    
+    data = np.zeros((num_small + num_big, imageL, imageW, 3))     # Channels = 3
+    for i in range(0, num_small): 
         fileName = files[i]
-        x = imread(myPath + fileName, flatten=False, mode='RGB')    # returns ndarray (L x W x 3)
+        x = imread(small_path + fileName, flatten=False, mode='RGB')    # returns ndarray (L x W x 3)
         x = imresize(x, (224,224))
         data[i,:,:,:] = x
+    files = os.listdir(big_path)
+    for i in range(num_small, num_small + num_big):
+        fileName = files[i - num_small]
+        x = imread(big_path + fileName, flatten=False, mode='RGB')    # returns ndarray (L x W x 3)
+        x = imresize(x, (224,224))
+        data[i,:,:,:] = x  
+
     data = data.transpose(0,3,1,2)       # [n, 224, 224, 3] --> [n, 3, 224, 224]
 
     # Load VGG16 weights and trained FC weights into the model
@@ -115,16 +133,17 @@ if __name__ == "__main__":
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
     identifier = 0
-    predictions = np.zeros(numImages,2)
+    predictions = np.zeros((num_small,2))
     p_file = 'predictions' + str(identifier) + '.csv'
-    for i in range(0, numImages):
+    for i in range(0, num_small):
         image = data[i,:,:,:]
         image = image.reshape([1, 3, 224, 224])
         out = model.predict(image)
-        predict = argmax(out)
+        predict = np.argmax(out)
         print(predict)
-        predictions(i,:) = [i + 1, predict]
-        numpy.savetxt(p_file, a, delimiter=",")
+        predictions[i,:] = [int(i + 1), int(predict + 1)]
+
+    np.savetxt(p_file, predictions, fmt='%d', delimiter=",")
     with open(p_file, 'r+') as f:
         content = f.read()
         f.seek(0, 0)
